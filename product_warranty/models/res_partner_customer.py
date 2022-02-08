011dac8ed02677074aa1fd7b36184aafd3cc7741
vu <tranlongvu_t63@hus.edu.vn>
2022-02-08 11:15:57 +0700

    edit sale order override func

--- product_warranty/models/res_partner_customer.py
+++ product_warranty/models/res_partner_customer.py
@@ -1,4 +1,3 @@
 from odoo import models , fields ,api
 class resPartnerCustomer(models.Model):
-    # _name = 'res.partner.customer'
     _inherit ='res.partner'
--- product_warranty/models/sale_order_warranty.py
+++ product_warranty/models/sale_order_warranty.py
@@ -11,20 +11,14 @@ class SaleOrderWarranty(models.Model):
         for record in self:
             record.discount_estimated = record.product_id.sale_order_discount_estimated * record.product_uom_qty
 
+
+
     @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id')
     def _compute_amount(self):
-        """
-        Compute the amounts of the SO line.
-        """
+        res = super(SaleOrderWarranty,self)._compute_amount()
         for line in self:
-            price = line.price_unit * (1 - (line.discount or 0.0) / 100.0) - line.discount_estimated/line.product_uom_qty
-            taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty,
-                                            product=line.product_id, partner=line.order_id.partner_shipping_id)
-            line.update({
-                'price_tax': taxes['total_included'] - taxes['total_excluded'],
-                'price_total': taxes['total_included'],
-                'price_subtotal': taxes['total_excluded'],
-            })
-            if self.env.context.get('import_file', False) and not self.env.user.user_has_groups(
-                    'account.group_account_manager'):
-                line.tax_id.invalidate_cache(['invoice_repartition_line_ids'], [line.tax_id.id])
+            line.price_subtotal -= line.discount_estimated
+        return res
+
+
+
